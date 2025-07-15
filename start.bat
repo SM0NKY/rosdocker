8acda2ad1dd35cdb32bb10a4454fd5c484053dc5
SM0NKY <smonky123.9.ki@hotmail.com>
2025-07-15 00:16:58 -0700

    Generalización de Docker

--- start.bat
+++ start.bat
@@ -8,10 +8,21 @@ set ip=%ip:~1%
 
 echo Usando DISPLAY=%ip%:0
 
-REM Lanzar el contenedor Docker con DISPLAY configurado
+REM Obtener directorio del script (con barra invertida al final)
+set SCRIPT_DIR=%~dp0
+
+REM Cambiar \ a / para la ruta Docker (opcional, Docker suele aceptar \ pero / es más seguro)
+set MOUNT_DIR=%SCRIPT_DIR:\=/%
+
+REM Añadir ros2_wks al final
+set MOUNT_DIR=%MOUNT_DIR%ros2_wks
+
+echo Montando volumen local: %MOUNT_DIR%
+
+REM Lanzar el contenedor Docker con DISPLAY configurado y volumen dinámico
 docker run -it --rm ^
   -e DISPLAY=%ip%:0 ^
   -e QT_X11_NO_MITSHM=1 ^
-  -v "E:/UABC/CIUDSE/Rover - Software/PruebasDocker/ROS_HUMBLE/ros2_wks:/root/ros2_ws" ^
+  -v "%MOUNT_DIR%:/root/ros2_ws" ^
   -v /tmp/.X11-unix:/tmp/.X11-unix ^
-  ros2_hum1
+  ros2_hum1 & REM ros2_humb es el nombre del build de docker, se puede cambiar a uno diferente
